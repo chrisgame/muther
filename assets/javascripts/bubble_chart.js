f3aa5bfa6a4aa028931e7cdca9e4aa02be7b8d27
user <user@my-dev-vm.(none)>
2013-04-30 08:51:24 +0100

    sorted out initial state using the force!

--- assets/javascripts/bubble_chart.js
+++ assets/javascripts/bubble_chart.js
@@ -1,4 +1,4 @@
-var dataset=[]
+var dataset={nodes:[], links:[]}
 var startRadius = 100
 var minRadius = 9
 var maxRadius = 300
@@ -16,63 +16,162 @@ var xAxis;
 var yAxis;
 var xAxisGroup;
 var yAxisGroup;
+var force;
+var nodes;
+var text;
 
-function updateScales(){
-  xScale = d3.scale.linear()
-                   .domain([0, d3.max(dataset, function(d) {return d.pageLoadTime;})])
-                   .range([padding, w - padding])
-                   .clamp(true);
+var bubbles = {
+  updateScales: function(){
+    xScale = d3.scale.linear()
+                     .domain([0, d3.max(dataset.nodes, function(d) {return d.pageLoadTime;})])
+                     .range([padding, w - padding])
+                     .clamp(true);
 
-  yScale = d3.scale.linear()
-                   .domain([0, d3.max(dataset, function(d) {return d.apdex;})])
-                   .range([padding, h - padding])
-                   .clamp(true);
+    yScale = d3.scale.linear()
+                     .domain([0, d3.max(dataset.nodes, function(d) {return d.apdex;})])
+                     .range([padding, h - padding])
+                     .clamp(true);
 
-  rScale = d3.scale.linear()
-                   .domain([0,
-                            d3.max(dataset, function(d) {return d.uniqueVisitors;})])
-                   .range([minRadius, maxRadius])
-                   .clamp(true);
-}
+    rScale = d3.scale.linear()
+                      .domain([0, d3.max(dataset.nodes, function(d) {return d.uniqueVisitors;})])
+                      .range([minRadius, maxRadius])
+                      .clamp(true);
+  },
+  updateAxis: function(){
+    xAxis = d3.svg.axis()
+                  .scale(xScale)
+                  .orient('bottom')
+                  .ticks(5)
 
-function updateAxis(){
-  xAxis = d3.svg.axis()
-                .scale(xScale)
-                .orient('bottom')
-                .ticks(5)
+    xAxisGroup.call(xAxis)
+              .attr("transform", "translate(0," + (h - padding) + ")");
 
-  xAxisGroup.call(xAxis)
-            .attr("transform", "translate(0," + (h - padding) + ")");
+    yAxis = d3.svg.axis()
+                  .scale(yScale)
+                  .orient('left')
+                  .ticks(5)
 
-  yAxis = d3.svg.axis()
-                .scale(yScale)
-                .orient('left')
-                .ticks(5)
+    yAxisGroup.call(yAxis)
+              .attr("transform", "translate(" + padding + ",0)");
+  },
+  updateLabels: function(){
 
-  yAxisGroup.call(yAxis)
-            .attr("transform", "translate(" + padding + ",0)");
-}
+    svg.selectAll('text')
+       .data(dataset.nodes)
+       .attr('x', function(d, i){
+         return xScale(d.pageLoadTime);
+       }) 
+       .attr('y', function(d, i){
+         return yScale(d.apdex);
+       })
+       .attr('text-anchor', 'middle')
+       .attr('fill', 'black');
+  },
+  updateForce: function(){
 
-function updateLabels(){
+    force = d3.layout.force()
+		  	 .nodes(dataset.nodes)
+	               	 .charge(function(d){
+		         	0 - (rScale(d.uniqueVisitors) * 1.5)
+	               	  })
+                         .gravity([0.03])
+	                 .size([w, h])
+	                 .start();
+  },
+  update: function(){
 
-  svg.selectAll('text')
-     .data(dataset)
-     .text(function(d, i){
-       return formating.prettyText(d.name)
-     })
-     .attr('x', function(d, i){
-       return xScale(d.pageLoadTime);
-     }) 
-     .attr('y', function(d, i){
-       return yScale(d.apdex) - (i*10);
-     })
-     .attr('text-anchor', 'middle')
-     .attr('fill', 'black');
-}
+    force.on('tick', function(){
+	  nodes.attr('cx', function(d){return d.x;})
+               .attr('cy', function(d){return d.y;})
+          bubbles.updateScales();
+          bubbles.updateLabels();
+    });
 
+    bubbles.updateScales();
+    bubbles.updateAxis();
+    bubbles.updateForce();
+    svg.selectAll('circle')
+       .data(dataset.nodes)
+       .transition()
+       .duration('1000')
+       .each('start', function(d, i){
+         if(i == 0){
+           console.log('A');
+         }
+       })
+       .attr('class', function(d, i){
+         return 'build-status-'+d.buildStatus
+       })
+       .attr('r', function(d, i){
+         return rScale(d.uniqueVisitors);
+       })
+       .attr('cx', function(d, i){
+         return xScale(d.pageLoadTime);
+       }) 
+       .attr('cy', function(d, i){
+         return yScale(d.apdex);
+       })
+       .each('end', function(d, i){
+        if(i == dataset.nodes.length-1){
+          console.log('F');
+        }
+       })
+    bubbles.updateLabels();
+  },
+  initialize: function(){
+
+    force = d3.layout.force()
+              .nodes(dataset.nodes)
+              .charge([-500])
+	      .gravity([0.03])
+	      .size([w, h])
+	      .start();
+
+    nodes.attr('r', startRadius)
+         .attr('cx', function(d, i){
+           return (i * 200)+startRadius
+         }) 
+         .attr('cy', 200)
+         .attr('fill', 'grey')
+         .attr('stroke', 'lightgrey')
+         .attr('stroke-width', '5px')
+	 .call(force.drag);
+      
+    text = svg.selectAll('text')
+              .data(dataset.nodes)
+              .enter()
+              .append('text')
+              .text(function(d, i){
+                return d.name
+              })
+              .attr('x', function(d, i){
+                return d.x;
+              })
+              .attr('y', function(d, i){
+	        return d.y;
+              })
+              .attr('text-anchor', 'middle')
+              .attr('fill', 'white');
+
+    force.on('tick', function(){
+      nodes.attr('cx', function(d){return d.x;})
+           .attr('cy', function(d){return d.y;})
+ 
+      text.text(function(d, i){
+            return formating.prettyText(d.name)
+          })
+	    .attr('x', function(d, i){
+            return d.x;
+          }) 
+          .attr('y', function(d, i){
+	    return d.y;
+          })
+    });
+  }  
+};
 $(function(){
   $.each(sites, function(key, value){
-    dataset.push({'name' : value, 'uniqueVisitors' : startRadius, 'pageLoadTime' : defaultPageLoadTime, 'apdex' : defaultApdex})
+    dataset.nodes.push({'name' : value, 'uniqueVisitors' : startRadius, 'pageLoadTime' : defaultPageLoadTime, 'apdex' : defaultApdex})
   });
 
 
@@ -82,43 +181,27 @@ $(function(){
           .attr('width', w)
           .attr('height', h);
 
-  var circles = svg.selectAll('circle')
-                    .data(dataset)
-                    .enter() 
-                    .append('circle');
+  nodes = svg.selectAll('circle')
+	     .data(dataset.nodes)
+	     .enter() 
+	     .append('circle');
 
-  circles.attr('r', startRadius)
-         .attr('cx', function(d, i){
-           return (i * 200)+startRadius
-         }) 
-         .attr('cy', 200)
-         .attr('fill', 'grey')
-         .attr('stroke', 'lightgrey')
-         .attr('stroke-width', '5px')
+  force = d3.layout.force()
+	           .nodes(dataset.nodes)
 
-  var text = svg.selectAll('text')
-                .data(dataset)
-                .enter()
-                .append('text')
-                .text(function(d, i){
-                  return d.name
-                })
-                .attr('x', function(d, i){
-                  return (i * 200)+100
-                })
-                .attr('y', 200)
-                .attr('text-anchor', 'middle')
-                .attr('fill', 'white');
 
   xAxisGroup = svg.append('g')
                   .attr('class', 'axis');
   yAxisGroup = svg.append('g')
                   .attr('class', 'axis');
 
-  deferedFetch.updateUniqueVisitorsFromGoogleAnalytics(dataset, timePoints.startOfYesterday(), timePoints.endOfYesterday())
-  deferedFetch.updatePageLoadTimeFromGoogleAnalytics(dataset, timePoints.startOfYesterday(), timePoints.endOfYesterday())
-  deferedFetch.updateApdexFromNewRelic(dataset, timePoints.oneHourAgo(), timePoints.currentDateTime())
-  deferedFetch.updateBuildStatusFromTeamCity(dataset);
+
+  bubbles.initialize();
+
+//  deferedFetch.updateUniqueVisitorsFromGoogleAnalytics(dataset, timePoints.startOfYesterday(), timePoints.endOfYesterday())
+//  deferedFetch.updatePageLoadTimeFromGoogleAnalytics(dataset, timePoints.startOfYesterday(), timePoints.endOfYesterday())
+//  deferedFetch.updateApdexFromNewRelic(dataset, timePoints.oneHourAgo(), timePoints.currentDateTime())
+//  deferedFetch.updateBuildStatusFromTeamCity(dataset);
 
   window.setInterval(function(){deferedFetch.updateUniqueVisitorsFromGoogleAnalytics(dataset, timePoints.startOfYesterday(), timePoints.endOfYesterday())}, 86400000);
   window.setInterval(function(){deferedFetch.updatePageLoadTimeFromGoogleAnalytics(dataset, timePoints.startOfYesterday(), timePoints.endOfYesterday())}, 86400000);
--- assets/javascripts/defered_fetch.js
+++ assets/javascripts/defered_fetch.js
@@ -1,60 +1,60 @@
 
 var deferedFetch = {
   fetchUniqueVisitorsFromGoogleAnalytics: function(dataset, startDate, endDate, pos){
-    var fetchUrl = 'http://localhost:3000/google-analytics.json?site='+dataset[pos].name+'&startdate='+formating.urlDate(startDate)+'&enddate='+formating.urlDate(endDate);
+    var fetchUrl = 'http://localhost:3000/google-analytics.json?site='+dataset.nodes[pos].name+'&startdate='+formating.urlDate(startDate)+'&enddate='+formating.urlDate(endDate);
     $.getJSON(fetchUrl, function(data){
       try{
-        console.log('R '+dataset[pos].name+' returned '+data.site.unique_visitors); 
-        dataset[pos].uniqueVisitors = parseInt(data.site.unique_visitors);
+        console.log('R '+dataset.nodes[pos].name+' returned '+data.site.unique_visitors); 
+        dataset.nodes[pos].uniqueVisitors = parseInt(data.site.unique_visitors);
       }catch(error){
-        dataset[pos].uniqueVisitors = minRadius;        
+        dataset.nodes[pos].uniqueVisitors = minRadius;        
       }
-      deferedFetch.updateBubbles(dataset, startDate, endDate, pos);
+      bubbles.update(dataset, startDate, endDate, pos);
     });
   },
 
   fetchPageLoadTimeFromGoogleAnalytics: function(dataset, startDate, endDate, pos){
-    var fetchUrl = 'http://localhost:3000/google-analytics.json?site='+dataset[pos].name+'&startdate='+formating.urlDate(startDate)+'&enddate='+formating.urlDate(endDate);
+    var fetchUrl = 'http://localhost:3000/google-analytics.json?site='+dataset.nodes[pos].name+'&startdate='+formating.urlDate(startDate)+'&enddate='+formating.urlDate(endDate);
     $.getJSON(fetchUrl, function(data){
       try{
-        console.log('R '+dataset[pos].name+' returned '+data.site.average_page_load_time); 
-        dataset[pos].pageLoadTime = parseFloat(data.site.average_page_load_time);
+        console.log('R '+dataset.nodes[pos].name+' returned '+data.site.average_page_load_time); 
+        dataset.nodes[pos].pageLoadTime = parseFloat(data.site.average_page_load_time);
       }catch(error){
-        dataset[pos].pageLoadTime = defaultPageLoadTime;        
+        dataset.nodes[pos].pageLoadTime = defaultPageLoadTime;        
       }
-      deferedFetch.updateBubbles(dataset, startDate, endDate, pos);
+      bubbles.update(dataset, startDate, endDate, pos);
     });
   },
 
   fetchApdexFromNewRelic: function(dataset, startDate, endDate, pos){
-    var fetchUrl = 'http://localhost:3000/new-relic.json?site='+dataset[pos].name+'&startdate='+formating.urlDate(startDate)+'&enddate='+formating.urlDate(endDate);
+    var fetchUrl = 'http://localhost:3000/new-relic.json?site='+dataset.nodes[pos].name+'&startdate='+formating.urlDate(startDate)+'&enddate='+formating.urlDate(endDate);
     $.getJSON(fetchUrl, function(data){
       try{
-        console.log('R '+dataset[pos].name+' returned '+data.site.apdex); 
-        dataset[pos].apdex = parseFloat(data.site.apdex);
+        console.log('R '+dataset.nodes[pos].name+' returned '+data.site.apdex); 
+        dataset.nodes[pos].apdex = parseFloat(data.site.apdex);
       }catch(error){
-        dataset[pos].apdex = defaultApdex;        
+        dataset.nodes[pos].apdex = defaultApdex;        
       }
-      deferedFetch.updateBubbles(dataset, startDate, endDate, pos);
+      bubbles.update(dataset, startDate, endDate, pos);
     });
   },
 
   fetchBuildStatusFromTeamCity: function(dataset, pos){
-    var fetchUrl = 'http://localhost:3000/team-city.json?site='+dataset[pos].name+'&startdate=2013-01-31T00:00&enddate=2013-02-01T00:00';
+    var fetchUrl = 'http://localhost:3000/team-city.json?site='+dataset.nodes[pos].name+'&startdate=2013-01-31T00:00&enddate=2013-02-01T00:00';
     $.getJSON(fetchUrl, function(data){
       try{
-        console.log('R '+dataset[pos].name+' returned '+data.site.build_status); 
-        dataset[pos].buildStatus = data.site.build_status;
+        console.log('R '+dataset.nodes[pos].name+' returned '+data.site.build_status); 
+        dataset.nodes[pos].buildStatus = data.site.build_status;
       }catch(error){
-        dataset[pos].buildStatus = defaultBuildStatus;        
+        dataset.nodes[pos].buildStatus = defaultBuildStatus;        
       }
-      deferedFetch.updateBubbles(dataset, pos);
+      bubbles.update(dataset, pos);
     });
   },
 
   updateUniqueVisitorsFromGoogleAnalytics: function(dataset, startDate, endDate){
     var fetchArray = [];
-    for (i = 0; i < dataset.length; i += 1){
+    for (i = 0; i < dataset.nodes.length; i += 1){
         fetchArray.push(deferedFetch.fetchUniqueVisitorsFromGoogleAnalytics(dataset, startDate, endDate, i));
     };
     $.when.apply($, fetchArray).done(function(){
@@ -64,7 +64,7 @@ var deferedFetch = {
 
   updatePageLoadTimeFromGoogleAnalytics: function(dataset, startDate, endDate){
     var fetchArray = [];
-    for (i = 0; i < dataset.length; i += 1){
+    for (i = 0; i < dataset.nodes.length; i += 1){
         fetchArray.push(deferedFetch.fetchPageLoadTimeFromGoogleAnalytics(dataset, startDate, endDate, i));
     };
     $.when.apply($, fetchArray).done(function(){
@@ -74,7 +74,7 @@ var deferedFetch = {
 
   updateApdexFromNewRelic: function(dataset, startDate, endDate){
     var fetchArray = [];
-    for (i = 0; i < dataset.length; i += 1){
+    for (i = 0; i < dataset.nodes.length; i += 1){
         fetchArray.push(deferedFetch.fetchApdexFromNewRelic(dataset, startDate, endDate, i));
     };
     $.when.apply($, fetchArray).done(function(){
@@ -84,46 +84,11 @@ var deferedFetch = {
 
   updateBuildStatusFromTeamCity: function(dataset){
     var fetchArray = [];
-    for (i = 0; i < dataset.length; i += 1){
+    for (i = 0; i < dataset.nodes.length; i += 1){
         fetchArray.push(deferedFetch.fetchBuildStatusFromTeamCity(dataset, i));
     };
     $.when.apply($, fetchArray).done(function(){
       console.log('Finished Build Status From Team City') 
     });
   },
-
-  updateBubbles: function(dataset, startDate, endDate, pos){
-    updateScales();
-    updateAxis();
-    updateLabels();
-    svg.selectAll('circle')
-       .data(dataset)
-       .transition()
-       .duration('1000')
-       .each('start', function(d, i){
-         if(i == 0){
-           console.log('A');
-         }
-       })
-       .attr('class', function(d, i){
-         return 'build-status-'+d.buildStatus
-       })
-       .attr('r', function(d, i){
-         return rScale(d.uniqueVisitors);
-       })
-       .attr('cx', function(d, i){
-         return xScale(d.pageLoadTime);
-       }) 
-       .attr('cy', function(d, i){
-         return yScale(d.apdex);
-       })
-       .each('end', function(d, i){
-        if(i == dataset.length-1){
-          console.log('F');
-          if(pos < dataset.length-1){
-            console.log('F All Anim')
-          }
-        }
-       });
-  }
 };
