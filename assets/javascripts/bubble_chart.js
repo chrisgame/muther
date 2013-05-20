e7401dea77f0808b57bb91f9383c865f72288e80
user <user@my-dev-vm.(none)>
2013-05-20 09:03:24 +0100

    basic force model with sites all added to the first group with fixed radius

--- assets/javascripts/bubble_chart.js
+++ assets/javascripts/bubble_chart.js
@@ -1,5 +1,7 @@
 var dataset={nodes:[], links:[]}
 var startRadius = 100
+var startX = 100
+var startY = 100
 var minRadius = 9
 var maxRadius = 300
 var defaultApdex = 0
@@ -68,202 +70,116 @@ var bubbles = {
 	  return d.y;
 	})
 	.attr('fill', 'black');
-  },
-  updateForce: function(){
-
-    force.nodes(dataset.nodes)
-	 .each()
-	 .charge(function(d){
-	   0 - (rScale(d.uniqueVisitors) * 1.5)
-	 })
-         .gravity([0.0000000000000015])
-
-  },
-  overlap: function(node, otherNode){
-    if (node.name == otherNode.name){return};
-    debugger;
-    var r = node.r,
-	lowerXLimit = node.x - r,
-	upperXLimit = node.x + r,
-	lowerYLimit = node.y - y,
-	upperYLimit = node.y + y;
-
-    return otherNode.x > 
-
-  },
-  overlappingNodes: function(node){
-    var i = 0,
-        n = dataset.nodes.length,
-	result = [];
-
-    while (++i < n ){
-      var otherNode = dataset.nodes[i];
-      if (bubbles.overlap(node, otherNode)){
-        result.push(otherNode);
-      }	
-    }
-  },
-  getFoci: function(node){
-    var i = 0, 
-        overlappingNodes = bubbles.overlappingNodes(node);
-    while (++i < overlappingNodes.length){
-      if (node.r > overlappingNode[i].r){
-        return  true;
-      }
-      else{ 
-        return overlapingNodes.sort(function(a, b){a.r - b.r})[0]  
-      }
-    }
-  },
-  updateNode: function(node){
-    node.r = rScale(node.uniqueVisitors);
-    node.x = xScale(node.pageLoadTime);
-    node.y = yScale(node.apdex);
-    node.foci = bubbles.getFoci(node);
-  },
-  updateDataset: function(){
-    var i = 0,
-        n = dataset.nodes.length;
-
-    while (++i < n ) bubbles.updateNode(dataset.nodes[i]);
-  },
-  update: function(){
-  
-    force.stop 
-    bubbles.updateScales();
-    bubbles.updateAxis();
-    bubbles.updateDataset();
-
-    svg.selectAll('circle')
-       .data(dataset.nodes)
-       .transition()
-       .duration('1000')
-       .each('start', function(d, i){
-         if(i == 0){
-           console.log('A');
-	 }
-       })
-       .attr('data-name', function(d, i){
-         return d.name
-       })
-       .attr('class', function(d, i){
-         return 'build-status-'+d.buildStatus
-       })
-       .attr('r', function(d, i){
-         return d.r
-       })
-       .attr('cx', function(d, i){
-	 return d.x
-       }) 
-       .attr('cy', function(d, i){
-	 return d.y
-       })
-       .each('end', function(d, i){
-         bubbles.updateLabels();
-
-         if(i == dataset.length-1){
-	   console.log('F');
-	   if(pos < dataset.length-1){
-	     console.log('F All Anim')
-           }
-         }
-       });
-
-
-  },
-  initialize: function(){
-
-    force = d3.layout.force()
-              .nodes(dataset.nodes)
-              .charge([-500])
-	      .gravity([0.03])
-	      .size([w, h])
-	      .start();
-
-    nodes.attr('r', startRadius)
-         .attr('cx', function(d, i){
-           return (i * 200)+startRadius
-         }) 
-         .attr('cy', 200)
-         .attr('fill', 'grey')
-         .attr('stroke', 'lightgrey')
-         .attr('stroke-width', '5px')
-	 .call(force.drag);
-      
-    text = svg.selectAll('text')
-              .data(dataset.nodes)
-              .enter()
-              .append('text')
-              .text(function(d, i){
-                return d.name
-              })
-              .attr('x', function(d, i){
-                return d.x;
-              })
-              .attr('y', function(d, i){
-	        return d.y;
-              })
-              .attr('text-anchor', 'middle')
-              .attr('fill', 'white');
-
-    force.on('tick', function(){
-      nodes.attr('cx', function(d){return d.x;})
-           .attr('cy', function(d){return d.y;})
- 
-      text.text(function(d, i){
-            return formating.prettyText(d.name)
-          })
-	    .attr('x', function(d, i){
-            return d.x;
-          }) 
-          .attr('y', function(d, i){
-	    return d.y;
-          })
-    });
-  }  
+  }
 };
-$(function(){
-  $.each(sites, function(key, value){
-    dataset.nodes.push({'name' : value, 'uniqueVisitors' : startRadius, 'pageLoadTime' : defaultPageLoadTime, 'apdex' : defaultApdex})
-  });
-
-
-  svg = d3.select('body')
-          .append('svg')
-          .attr('id', 'monitor')
-          .attr('width', w)
-          .attr('height', h);
-
-  nodes = svg.selectAll('circle')
-	     .data(dataset.nodes)
-	     .enter() 
-	     .append('circle');
-
-  force = d3.layout.force()
-	           .nodes(dataset.nodes)
-
-
-  xAxisGroup = svg.append('g')
-                  .attr('class', 'axis');
-  yAxisGroup = svg.append('g')
-                  .attr('class', 'axis');
-
-
-  bubbles.initialize();
-
-  window.setTimeout(function(){
-	  
-	  deferedFetch.updateUniqueVisitorsFromGoogleAnalytics(dataset, timePoints.startOfYesterday(), timePoints.endOfYesterday())
-	  deferedFetch.updatePageLoadTimeFromGoogleAnalytics(dataset, timePoints.startOfYesterday(), timePoints.endOfYesterday())
-	  deferedFetch.updateApdexFromNewRelic(dataset, timePoints.oneHourAgo(), timePoints.currentDateTime())
-	  deferedFetch.updateBuildStatusFromTeamCity(dataset);
-  }, 3000);
-
-  window.setInterval(function(){deferedFetch.updateUniqueVisitorsFromGoogleAnalytics(dataset, timePoints.startOfYesterday(), timePoints.endOfYesterday())}, 86400000);
-  window.setInterval(function(){deferedFetch.updatePageLoadTimeFromGoogleAnalytics(dataset, timePoints.startOfYesterday(), timePoints.endOfYesterday())}, 86400000);
-  window.setInterval(function(){deferedFetch.updateApdexFromNewRelic(dataset, timePoints.oneHourAgo(), timePoints.currentDateTime())}, 60000);
-  window.setInterval(function(){deferedFetch.updateBuildStatusFromTeamCity(dataset)}, 30000);
-
-});
   
+$(function(){
+  var nodes = d3.range(5).map(function(i) {
+    return {type: Math.random() * 5 | 0,
+	    r: 30,
+            fixed: true,
+            type: i,
+            x: (i+1) * (w / 6),
+            y: h / 2};
+  }),
+  color = d3.scale.category10();
 
+  var i = 0;
+
+  while (++i < sites.length){
+    nodes.push({'name': sites[i], 'fixed': false, 'type': 0, 'r': startRadius+i, 'x': startX+i, 'y': startY+i})
+  } 
+
+  var force = d3.layout.force()
+      .gravity(0)
+      .charge(0)
+      .nodes(nodes)
+      .size([w, h]);
+  
+  force.start();
+
+  var svg = d3.select('body')
+      .append('svg')
+      .attr('width', w)
+      .attr('height', h)
+
+  svg.append('svg:rect')
+      .attr('width', w)
+      .attr('height', h);
+
+  svg.selectAll('circle')
+      .data(nodes)
+      .enter().append('svg:circle')
+      .attr('r', function(d) {return d.r - 2})
+      .style('fill', function(d, i) {return color(d.type);});
+
+  force.on('tick', function(e){
+      var q = d3.geom.quadtree(nodes),
+	  k = e.alpha * .1,
+	  i = 0,
+	  n = nodes.length,
+	  o;
+
+      while (++i < n) {
+        o = nodes[i];
+	if (o.fixed) continue;
+	c = nodes[o.type];
+	o.x += (c.x - o.x) * k;
+	o.y += (c.y - o.y) * k;
+	q.visit(collide(o));
+      }
+
+      svg.selectAll('circle')
+	 .attr('cx', function(d) { return d.x; })
+	 .attr('cy', function(d) { return d.y; });
+
+
+	var p0;
+	svg.on('mousemove', function() {
+	  var p1 = d3.svg.mouse(this),
+	      node = {r:Math.random() * 12 + 4, type: Math.random() * 5 | 0, x: p1[0], y: p1[1], px: (p0 || (p0 = p1))[0], py: p0[1]};
+
+	  p0 = p1;
+
+	  svg.append('svg:circle')
+	      .data([node])
+	      .attr('cx', function(d) { return d.x; })
+	      .attr('cy', function(d) { return d.y; })
+	      .attr('r', function(d) { return d.r - 2; })
+	      .style('fill', function(d) { return color(d.type);})
+	      .transition()
+		.delay(3000)
+		.attr('r', 1e-6)
+		.each('end', function() { nodes.splice(6, 1); })
+		.remove();
+
+	   nodes.push(node);
+	   force.resume();
+	});
+
+	function collide(node) {
+	    var r = node.r,
+		nx1 = node.x - r,
+		nx2 = node.x + r,
+		ny1 = node.y - r,
+		ny2 = node.y + r;
+	    return function(quad, x1, y1, x2, y2) {
+	      if (quad.point && (quad.point !== node)) {
+		var x = node.x - quad.point.x,
+		    y = node.y - quad.point.y,
+		    l = Math.sqrt(x * x + y * y),
+		    r = node.r + quad.point.r;
+		if (l < r) {
+		  l = (l - r) / l * .5;
+		  node.px += x * l;
+		  node.py += y * l;
+		}
+	      }
+	      return x1 > nx2
+		  || x2 < nx1
+		  || y1 > ny2
+		  || y2 < ny1;
+	    };
+        }
+  });
+});
